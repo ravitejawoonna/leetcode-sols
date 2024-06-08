17b739d72978420720b122907f32df5ec3ac510d
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-06-08 19:11:15 +0530

    Time: 32 ms (74.01%) | Memory: 16.4 MB (98.30%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for i in range(0, len(s)):
+            if s[i] != "]":
+                if not stack:
+                    stack.append(s[i])
+                    continue
+                
+                if (stack[-1].isdigit() and s[i].isdigit()) or (stack[-1].isalpha() and s[i].isalpha()):
+                    stack[-1] = stack[-1] + s[i]
+                    continue
+                stack.append(s[i])
+            else:
+                rep_str = ""
+                while stack[-1] != "[":
+                    rep_str = stack.pop() + rep_str
+                stack.pop()
+                times = stack.pop()
+                
+                stack.append(
+                    "".join([rep_str for _ in range(int(times))])
+                )
+        return "".join(stack)
