5e6d4c1d6a9a506e4ed2a90396c6355f9130b915
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-06-09 10:52:14 +0530

    Time: 59 ms (100.00%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.py
+++ 3422-find-the-n-th-value-after-k-seconds/find-the-n-th-value-after-k-seconds.py
@@ -1,8 +1,6 @@
 class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
-        vals = [1 for _ in range(n)]
-
-        for _ in range(k):
-            for i in range(1, n):
-                vals[i] = vals[i] + vals[i-1]
-        return vals[-1] % 1000000007
+        ans = 1
+        for i in range(1, n):
+            ans = (ans + (ans * k)//i) 
+        return ans % (10**9 + 7)
