09d55166c61deb71ac4b9c9eb1039dee80ffe1f5
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-07-12 21:02:56 +0530

    Time: 33 ms (68.25%) | Memory: 16.5 MB (14.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,8 +3,10 @@ class Solution:
         
         @cache
         def rec(n):
-            if n == 0 or n == 1:
-                return 1
+            if n <= 0: return 0
+            if n == 1: return 1
+            if n == 2: return 2
+
             res = rec(n-1) + rec(n-2)
             return res
 
