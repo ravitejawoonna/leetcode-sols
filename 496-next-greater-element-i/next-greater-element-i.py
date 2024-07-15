0116ec8fba5e5917d8f1dd027e8ed196d118ce9a
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-07-15 20:23:19 +0530

    Time: 39 ms (95.13%) | Memory: 16.7 MB (92.50%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,15 +1,21 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res = [-1] * len(nums1)
-        check = {
-            n:i for i, n in enumerate(nums1)
+        stack= []
+        cache = {
+            n:i for i,n in enumerate(nums1)
         }
-        stack = []
 
         for i in range(0, len(nums2)):
+
+            while not stack:
+                stack.append(nums2[i])
+                continue
+            
             while stack and stack[-1] < nums2[i]:
                 item = stack.pop()
-                res[check[item]] = nums2[i]
-            if nums2[i] in check:
+                if item in cache:
+                    res[cache[item]] = nums2[i]
+            if nums2[i] in cache:
                 stack.append(nums2[i])
         return res
