360ab462e8ffcbf38a28330befbe159707cdbe01
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-07-15 20:25:49 +0530

    Time: 153 ms (51.98%) | Memory: 18.3 MB (67.55%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,18 +1,15 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        # circular - do nums1 + nums1 to emulate circular
-
         stack = []
         res = [-1] * len(nums)
+
         for i in list(range(len(nums))) * 2:
             if not stack:
                 stack.append(i)
                 continue
             
             while stack and nums[stack[-1]] < nums[i]:
-                item = stack.pop()
-                res[item] = nums[i]
-            
+                res[stack[-1]] = nums[i]
+                stack.pop()
             stack.append(i)
-
         return res
