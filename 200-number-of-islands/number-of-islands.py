a342abfd210f6361910ca918a24eee37e1e389cf
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-08-19 18:06:33 +0530

    Time: 233 ms (76.33%) | Memory: 18.8 MB (78.62%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def rec(i,j):
+        count = 0
+        m = len(grid)
+        n = len(grid[0])
+
+        def scout(i,j):
             if (
                 i < 0 or
-                j < 0 or
                 i >= len(grid) or
+                j < 0 or
                 j >= len(grid[0]) or
                 grid[i][j] != "1"
             ):
-                
-                return
-                
-            grid[i][j] = "#"
-            rec(i+1, j)
-            rec(i-1, j)
-            rec(i,j+1)
-            rec(i,j-1)
+                return 
 
-        count = 0
-        for i in range(0, len(grid)):
-            for j in range(0, len(grid[0])):
+            grid[i][j] = "#"
+            scout(i+1,j)
+            scout(i-1, j)
+            scout(i, j+1)
+            scout(i, j-1)
+        
+        for i in range(0, m):
+            for j in range(0, n):
                 if grid[i][j] == "1":
-                    rec(i,j)
                     count += 1
-        return count           
+                    scout(i,j)
+        return count            
