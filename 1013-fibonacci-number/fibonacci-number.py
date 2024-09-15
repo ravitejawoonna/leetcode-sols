03fcdf23ae00111976a8715e43e0f15aa7daa797
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-09-15 08:52:18 +0530

    Time: 35 ms (68.99%) | Memory: 16.6 MB (9.95%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,6 +1,17 @@
 class Solution:
     @cache
     def fib(self, n: int) -> int:
+        # < ---- recursive 2 ^ N ---- >
+        # if n < 2:
+        #     return n
+        # return self.fib(n-1) + self.fib(n-2)
+
+
+        # < ---- Top down with memoization ---- >
+        cache = {}
         if n < 2:
             return n
-        return self.fib(n-1) + self.fib(n-2)
+        if n in cache:
+            return cache[n]
+        cache[n] = self.fib(n-1) + self.fib(n-2)
+        return cache[n]
