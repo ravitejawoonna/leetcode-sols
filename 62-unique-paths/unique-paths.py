d1bbee1b0cc4e5186f9a96dc94a44f7a00109d9b
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-09-15 20:55:25 +0530

    Time: 35 ms (59.55%) | Memory: 16.5 MB (39.37%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,13 +1,28 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def rec(i,j):
-            if i >= m or j >= n:
-                return 0
-            if i == m-1 and j == n-1:
-                return 1
+        # < ---- Recursive, Brute force ----- >
+        # @cache
+        # def rec(i,j):
+        #     if i >= m or j >= n:
+        #         return 0
+        #     if i == m-1 and j == n-1:
+        #         return 1
             
-            res = rec(i+1,j) + rec(i,j+1)
-            return res
-        return rec(0,0)
+        #     res = rec(i+1,j) + rec(i,j+1)
+        #     return res
+        # return rec(0,0)
+
+        dp = [
+            [0 for _ in range(n)] for _ in range(m)
+        ]
+        dp[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if i - 1 >= 0:
+                    dp[i][j] += dp[i-1][j]
+                if j - 1 >= 0:
+                    dp[i][j] += dp[i][j-1]
+        return dp[-1][-1]
+
         
