77c270a27493a08b0b86942c086b2afdfbdc2e99
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-09-15 11:06:11 +0530

    Time: 31 ms (77.92%) | Memory: 16.5 MB (61.78%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,13 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        @cache
-        def rec(n):
-            if n <= 0: return 0
-            if n == 1: return 1
-            if n == 2: return 2
-
-            res = rec(n-1) + rec(n-2)
-            return res
+        if n < 3:
+            return n
         
-        return rec(n)
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
