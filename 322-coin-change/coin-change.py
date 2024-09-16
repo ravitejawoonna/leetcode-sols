350044c6592274c29eba317808ec7cbbf8fa7bbd
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-09-16 16:01:31 +0530

    Time: 851 ms (39.08%) | Memory: 17.4 MB (32.71%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,14 +1,39 @@
 class Solution:
-    def coinChange(self, coins, amount):
-        
-        @lru_cache(maxsize=None)  
-        def dfs(curr):
-            if curr > amount: 
-                return math.inf
-            if curr == amount: 
-                return 0
-            return min(dfs(curr + val) + 1 for val in coins)
-                
-        result = dfs(0)
-        
-        return -1 if result == math.inf else result
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # < ---- brute force ----- >
+        # @cache
+        # def rec(s, n):
+        #     if s > amount:
+        #         return math.inf
+
+        #     if s == amount:
+        #         return n
+
+        #     return min(
+        #         [
+        #             rec(s+i, n+1) for i in coins
+        #         ]
+        #     )
+        # res = rec(0,0)
+        # return -1 if res == math.inf else res
+
+        # < ---- Bottom up tabuliztion ---- >
+        dp = [0] * (amount + 1)
+
+        coins.sort()
+        # finding the minimum number of coins required for all amounts up until amount
+
+        for i in range(1, amount+1):
+            minn = float('inf')
+            for c in coins:
+                diff = i - c
+                if diff < 0:
+                    break
+                minn = min(
+                    minn, dp[diff]+1
+                )
+            dp[i] = minn
+        print(dp)
+        if dp[amount] < float('inf'):
+            return dp[amount]
+        return -1
