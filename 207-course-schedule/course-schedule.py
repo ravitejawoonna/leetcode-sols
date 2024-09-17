75ce4db4e0578cd52b5207e9c06ddc3f93fbe6fe
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-09-17 16:07:04 +0530

    Time: 112 ms (8.04%) | Memory: 18.7 MB (11.99%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {
+            i:[] for i in range(numCourses)
+        }
+
+        for d in prerequisites:
+            adj[d[0]].append(d[1])        
+        
+        visited = set()
+        print(adj)
+        def rec(c):
+            if c in visited:
+                return False
+            if adj[c] == []:
+                return True
+            
+            visited.add(c)
+            print(c)
+
+            for p in adj[c]:
+                if not rec(p): return False
+            visited.remove(c)
+            adj[c] = []
+            return True
+        
+        for i in range(0, numCourses):
+            if not rec(i): return False
+        return True
