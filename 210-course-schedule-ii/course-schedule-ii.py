4a11beed0a6da77d85400f3dfd5cef5b23a43368
Raviteja Woonna <47233738+ravitejawoonna@users.noreply.github.com>
2024-09-17 16:54:47 +0530

    Time: 76 ms (98.99%) | Memory: 18.6 MB (12.33%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -4,12 +4,13 @@ class Solution:
             i:[] for i in range(0,numCourses)
         }
 
-        for p in prerequisites:
-            adj[p[0]].append(p[1])
+        for d in prerequisites:
+            adj[d[0]].append(d[1])
         
         visited = set()
+        output = []
         cycle = set()
-        res = []
+
         def rec(c):
             if c in cycle:
                 return False
@@ -18,14 +19,14 @@ class Solution:
             
             cycle.add(c)
             for p in adj[c]:
-                if not rec(p):
-                    return False
-            res.append(c)
+                if not rec(p): return False
             cycle.remove(c)
             visited.add(c)
+            output.append(c)
             return True
         
         for c in range(0, numCourses):
             if not rec(c):
                 return []
-        return res
+        return output
+            
